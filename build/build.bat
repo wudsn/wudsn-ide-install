ad07b4197fe2a26bf78d7c17ba020e7a03a0a3e0
Peter Dell <peter.dell@web.de>
2022-12-28 02:38:55 +0100

    Add signing and UPX compressing for EXE file

--- build/build.bat
+++ build/build.bat
@@ -13,5 +13,15 @@ for /f "delims=" %%# in ('powershell get-date -format "{yyyy.MM.dd.HHmm}"') do @
 for /f "delims=" %%# in ('powershell get-date -format "{yyyy}"') do @set FILE_YEAR=%%#
 set COPYRIGHT="(c) %FILE_YEAR% Peter Dell"
 build\bat2exe /bat %BAT_FILE% /exe %EXE_FILE% /icon build\wudsn.ico ^
-              /x64 /workdir 0 /overwrite ^
-              /fileversion %FILE_VERSION%  /productname "WUDSN IDE" /productversion %PRODUCT_VERSION% /copyright %COPYRIGHT% /company WUDSN 
+              /x64 /workdir 0 /overwrite /upx ^
+              /fileversion %FILE_VERSION%  /productname "WUDSN IDE" /productversion %PRODUCT_VERSION% /copyright %COPYRIGHT% /company WUDSN
+              
+set SIGNTOOL=C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x86\signtool.exe
+set SIGNTOOL_PFX_FILE=build\build.pfx
+rem set SIGNTOOL_PFX_PASSWORD=
+if not defined SIGNTOOL_PFX_PASSWORD (
+  echo Environment variable SIGNTOOL_PFX_PASSWORD is not set.
+  exit /B 1
+)
+
+"%SIGNTOOL%" sign /f %SIGNTOOL_PFX_FILE% /p %SIGNTOOL_PFX_PASSWORD% /tr http://timestamp.digicert.com /td SHA256 /fd SHA256 %EXE_FILE%
