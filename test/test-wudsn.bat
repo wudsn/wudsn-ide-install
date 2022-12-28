0ea885b91aa072831ffc67a7d8b11db67748852a
Peter Dell <peter.dell@web.de>
2022-12-28 02:38:23 +0100

    Add error handling

--- test/test-wudsn.bat
+++ test/test-wudsn.bat
@@ -2,11 +2,17 @@
 cd /D "%~dp0"
 set TEST_DIR=..\out\wudsn
 call ..\build\build.bat
-copy ..\wudsn.exe %TEST_DIR%
+if ERRORLEVEL 1 (
+  echo Build failed.
+  pause
+  exit /B 1
+)
 if not exist %TEST_DIR% mkdir %TEST_DIR%
+copy ..\wudsn.exe %TEST_DIR%
 pushd %TEST_DIR%
 
-rem set DOWNLOADS_URL
+rem set SITE_URL=http://localhost:8080
+rem set WUDSN_VERSION=daily
 wudsn.exe %*
 
 popd
