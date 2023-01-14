42a7d5fdc368d5b29ef27c159f5f27f5e125584d
Peter Dell <peter.dell@web.de>
2023-01-14 17:03:38 +0100

    Update test-wudsn-vm-ubuntu.bat

--- test/test-wudsn-vm-ubuntu.bat
+++ test/test-wudsn-vm-ubuntu.bat
@@ -1,8 +1,22 @@
 @echo off
-cd /D "%~dp0"
+rem
+rem The template was downloaded from https://www.osboxes.org/ubuntu/.
+rem Changes:
+rem - Install German keyboard layout
+rem - Add default aliases to /etc/profile
+rem - Remove not required apps from favorites
+
 echo Creating Clean Virtual Box Image from Template
 set TO=Ubuntu 22.04 (64bit)
 set FROM=%TO% - Template
+if not exist "%FROM%" (
+  chdir
+  echo ERROR: Folder "%FROM%" is not present in the current folder.
+  echo Make sure to run the script with the correct working directory.
+  pause
+  exit
+)
+
 rmdir /Q/S "%TO%
 mkdir "%TO%"
 copy /B "%FROM%\*" "%TO%"
