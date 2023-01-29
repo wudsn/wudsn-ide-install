df6c02b501486f06c4e1b9aa9d145c58dadd99ef
Peter Dell <peter.dell@web.de>
2023-01-29 15:55:11 +0100

    Update wudsn.bat

--- wudsn.bat
+++ wudsn.bat
@@ -164,9 +164,11 @@ rem
   if "%INSTALL_MODE%" == "" (
     if not exist "%PROJECTS_FOLDER%" (
       set INSTALL_MODE=--install-all-from-server
-    ) else if not exist "%INSTALL_FOLDER%" (
-      set INSTALL_MODE=--install
-      goto :eof
+    ) else (
+      if not exist "%INSTALL_FOLDER%" (
+        set INSTALL_MODE=--install
+        goto :eof
+      )
     )
   )
 
