b6ebd3da86c6a57fc78c67421aae7d9e6dfa7ac9
Peter Dell <peter.dell@web.de>
2023-02-05 21:25:10 +0100

    Support curl on macOS if wget is not installed

--- test/test-wudsn-clean-install.sh
+++ test/test-wudsn-clean-install.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 #
-# Test script to install both standard versions inside an Linux VM.
+# Test script to install both standard versions on macOS or inside a Linux VM.
 # Use update-test-wudsn-clean-install.sh to update this script from the server.
 #
 
-# Install the version in ${WUDSN_VERSION}
+# Download executable file from URL $1 to $2.
+download_executable(){
+  if -f "$2";
+  then
+  	rm -f "$2"
+  fi
+  if command -v curl &> /dev/null
+  then
+    curl --location "$1" --output "$2"
+  else
+    if command -v wget &> /dev/null
+    then
+      wget --no-cache "$1" -O "$2"
+    else
+      echo ERROR: Neither curl nor wget are installed.
+      exit 1
+    fi
+  fi
+  chmod a+x "$2"
+}
+
+# Install the version in ${WUDSN_VERSION}.
 install_wudsn(){
   echo Installing WUDSN IDE Version ${WUDSN_VERSION}.
   rm -rf "${WUDSN_VERSION}"
@@ -12,8 +33,7 @@ install_wudsn(){
   pushd "${WUDSN_VERSION}"
 
   echo Downloading Installer.
-  wget --no-cache "$INSTALLER_URL"
-  chmod a+x "${WUDSN_EXECUTABLE}"
+  download_executable "$INSTALLER_URL" "${WUDSN_EXECUTABLE}"
 
 # The following call must not be started in a new window, so sudo password inputs work and exits are ignored
   gnome-terminal --wait --title "Installing WUDSN version ${WUDSN_VERSION}" -- ./${WUDSN_EXECUTABLE} &
@@ -22,21 +42,20 @@ install_wudsn(){
 }
 
 install_wudsn_versions(){
-cd ~/jac || exit
+  cd ~/jac || exit
 
-WUDSN_EXECUTABLE=wudsn.sh
-INSTALLER_URL=https://github.com/peterdell/wudsn-ide-install/raw/main/${WUDSN_EXECUTABLE}
+  WUDSN_EXECUTABLE=wudsn.sh
+  INSTALLER_URL=https://github.com/peterdell/wudsn-ide-install/raw/main/${WUDSN_EXECUTABLE}
 
-rm -rf wudsn
-mkdir wudsn
-pushd wudsn
+  rm -rf wudsn
+  mkdir wudsn
+  pushd wudsn
 
-WUDSN_VERSION=daily
-install_wudsn
-WUDSN_VERSION=stable
-install_wudsn
-popd
-
+  WUDSN_VERSION=daily
+  install_wudsn
+  WUDSN_VERSION=stable
+  install_wudsn
+  popd
 }
 
 set -e
